a71b516ace220e2b7545d1621808f2410cfb294c
Weizq-Think <wzq@yeswzq.com>
2021-11-13 20:36:45 +0800

    修改

--- Day1-basic-1113/Random_ip.py
+++ Day1-basic-1113/Random_ip.py
@@ -8,5 +8,5 @@ ipaddress3=random.randint(1,254)
 ipaddress4=random.randint(1,254)
 
 print('%d.%d.%d.%d'%(ipaddress1,ipaddress2,ipaddress3,ipaddress4))  #方法1字符串格式化打印
-ipaddress = str(ipaddress1) + '.' + str(ipaddress2) + '.' + str(ipaddress3) + '.' + str(ipaddress4) #方法2转换成字符串格式后拼接
-print(ipaddress)
+#ipaddress = str(ipaddress1) + '.' + str(ipaddress2) + '.' + str(ipaddress3) + '.' + str(ipaddress4) #方法2转换成字符串格式后拼接
+#print(ipaddress)  #方法2

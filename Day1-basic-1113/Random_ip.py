c6bc7a9f7f49fafadc0c1e8b22fac2c6328c5504
Weizq-Think <wzq@yeswzq.com>
2021-11-15 10:51:56 +0800

    更新代码

--- Day1-basic-1113/Random_ip.py
+++ Day1-basic-1113/Random_ip.py
@@ -7,6 +7,16 @@ ipaddress2=random.randint(1,254)
 ipaddress3=random.randint(1,254)
 ipaddress4=random.randint(1,254)
 
-print('%d.%d.%d.%d'%(ipaddress1,ipaddress2,ipaddress3,ipaddress4))  #方法1字符串格式化打印
-#ipaddress = str(ipaddress1) + '.' + str(ipaddress2) + '.' + str(ipaddress3) + '.' + str(ipaddress4) #方法2转换成字符串格式后拼接
+#方法1字符串格式化打印
+print('%d.%d.%d.%d'%(ipaddress1,ipaddress2,ipaddress3,ipaddress4))
+
 #print(ipaddress)  #方法2
+#ipaddress = str(ipaddress1) + '.' + str(ipaddress2) + '.' + str(ipaddress3) + '.' + str(ipaddress4) #方法2转换成字符串格式后拼接
+
+
+#方法3,通过变量调用模块，然后直接拼接。
+ip = str(random.randint(0,255))+'.'+str(random.randint(0,255))+'.'\
+     +str(random.randint(0,255))+'.'+str(random.randint(0,255))
+print(ip)
+
+
---
+++ Day1-basic-1113/for.py
@@ -0,0 +1,3 @@
+#count10.py
+for i in range (10,0,-1):
+    print(i+2)

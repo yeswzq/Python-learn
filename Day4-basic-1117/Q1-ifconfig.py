00b263524ecb9b52ecea065d66609a29e6de504f
weizq <wzq@yeswzq.com>
2021-11-17 19:26:29 +0800

    update

---
+++ Day4-basic-1117/Q1-ifconfig.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+ifconfig_result = os.popen('ifconfig '+'ens33').read()
+
+addr = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',ifconfig_result)
+
+ipv4_addr = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',ifconfig_result)[0]
+netmask = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',ifconfig_result)[1]
+broadcast = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})',ifconfig_result)[2]
+mac_addr = re.findall(r'([0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})',ifconfig_result)[0]
+
+# format_string = '{ipv4_addr},{netmask},{broadcast},{mac_addr}'
+
+# print('mac-')
+# print(format_string.format(%(0)))
